2bf51a49d0eeefc1386aae9cb30548302b182199
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2014-12-01 17:47:43 +0100

    Begin tests

---
+++ Drinks/src/androidTest/java/fr/masciulli/drinks/test/MainActivityTest.java
@@ -0,0 +1,33 @@
+package fr.masciulli.drinks.test;
+
+import android.support.v4.view.ViewPager;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.SmallTest;
+import fr.masciulli.drinks.R;
+import fr.masciulli.drinks.activity.MainActivity;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    private MainActivity activity;
+    private boolean dualPane;
+    private ViewPager viewPager;
+
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        setActivityInitialTouchMode(false);
+        activity = getActivity();
+        dualPane = activity.getResources().getBoolean(R.bool.dualpane);
+        if (!dualPane) {
+            viewPager = (ViewPager) activity.findViewById(R.id.pager);
+        }
+    }
+
+    public void testPreconditions() {
+        assertNotNull(viewPager);
+        assertTrue(viewPager.getAdapter().getCount() == 2);
+    }
+}
